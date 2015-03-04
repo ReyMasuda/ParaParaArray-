a579782d7cfe035de0559688b6d02a981a8652a1
Rey <quit9909@icloud.com>
2015-03-05 00:23:30 +0900

    少し機能つけた

--- ParaParaArray/ViewController.h
+++ ParaParaArray/ViewController.h
@@ -8,7 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    IBOutlet UILabel *tittleLabel; //表示するlabel
+    IBOutlet UIImageView *imgView; //表示する画像
+    
+    int index; //表示する画像の番号
+    UIImage *imgArray[10]; //表示する画像の配列
+}
+
+-(IBAction)next:(id)sender; //ボタンを押した時の処理
 
 
 @end
--- ParaParaArray/ViewController.m
+++ ParaParaArray/ViewController.m
@@ -17,11 +17,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //配列に10枚の画像を入れる
+    imgArray[0] = [UIImage imageNamed:@"0dango.png"];
+    imgArray[1] = [UIImage imageNamed:@"1dango.png"];
+    imgArray[2] = [UIImage imageNamed:@"2dangos.png"];
+    imgArray[3] = [UIImage imageNamed:@"3dangos.png"];
+    imgArray[4] = [UIImage imageNamed:@"4dangos.png"];
+    imgArray[5] = [UIImage imageNamed:@"5dangos.png"];
+    imgArray[6] = [UIImage imageNamed:@"6dangos.png"];
+    imgArray[7] = [UIImage imageNamed:@"7dangos.png"];
+    imgArray[8] = [UIImage imageNamed:@"8dangos.png"];
+    imgArray[9] = [UIImage imageNamed:@"9dangos.png"];
+    
+    //最初に表示する画像を0番目に設定
+    index = 0;
+    
+    //imgViewに画像をセットする
+    [imgView setImage:imgArray[index]];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)next:(id)sender{
+    index = index +1; //indexの値を1増やす
+    
+    if (index > 2) {
+        index = 0;
+    }
 }
 
 @end
